022e41745ec22096b0a50bea2ac82d47028aa6eb
vicol <vicolv97@gmail.com>
2021-03-21 17:11:05 +0200

    task-3.6.4: fix null pointer exception

--- lib/my_stateless_widget.dart
+++ lib/my_stateless_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MyStateLessWidget extends StatelessWidget {
-  int _counter;
+  int _counter = 0;
   @override
   Widget build(BuildContext context) {
     print('print ${_counter++}');
