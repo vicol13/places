a12fde5c15f079f95bdf7fae153a5487b5708a13
vicol <vicolv97@gmail.com>
2021-03-25 15:54:13 +0200

    task/surf-4.4: add title to app bar

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'styled_title.dart';
 
 class SightListScreen extends StatefulWidget {
   @override
@@ -9,9 +10,16 @@ class _State extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: Center(
-         child: Text(' This is my custom scaffold'),
-       ) ,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70),
+        child: AppBar(
+          elevation: 0,
+          centerTitle: false,
+          backgroundColor: Colors.white, //??
+          title: StyledTitle('list of', 'interesting places'),
+        ),
+      ),
+      body: Center(child: Text('My first application')),
     );
   }
 }
