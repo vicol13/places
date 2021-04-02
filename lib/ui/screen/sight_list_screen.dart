c157c7d5f8c9ebf2d998274cc87e2d81e3407590
vicol <vicolv97@gmail.com>
2021-04-02 18:10:35 +0300

    surf/task-4.10: update main screen

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:places/ui/screen/sight_card.dart';
 import 'package:places/ui/screen/sight_detailed_screen.dart';
-import '../styled_title.dart';
+import '../widgets/styled_title.dart';
 import '../../mocks.dart' as mockdata;
 
 class SightListScreen extends StatefulWidget {
@@ -13,15 +13,16 @@ class _State extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     // return Scaffold(
-    //   body: SightDetails(mockdata.mocks[0]),
+      // body: SightDetails(mockdata.mocks[0]),
     // );
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(70),
-        child: Container(
-          alignment: Alignment.topLeft,
-          padding: EdgeInsets.only(top: 50,left: 30),
-          child: StyledTitle('list of', 'interesting places'),
+        child: AppBar(
+          elevation: 0,
+          centerTitle: false,
+          backgroundColor: Colors.white, //??
+          title: StyledTitle('list of', 'interesting places'),
         ),
       ),
       body: Center(
