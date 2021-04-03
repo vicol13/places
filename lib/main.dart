4de8cbc7581f178a658d538648e51b25a89c6c53
vicol <vicolv97@gmail.com>
2021-04-03 16:06:23 +0300

    task/surf-4.12: add bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:places/ui/screen/sight_list_screen.dart';
+import 'package:places/ui/screen/visiting_screen.dart';
 
 void main() {
   runApp(App());
@@ -10,12 +11,49 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Places',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SightListScreen(),
-    );
+        title: 'Places',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyApp()
+        );
   }
 }
 
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  TabController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 2, vsync: this);
+    _controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+          controller: _controller,
+          children: [SightListScreen(), VisitingScreen()]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _controller.index,
+        onTap: (curentIndex) {
+          _controller.animateTo(curentIndex);
+        },
+        selectedItemColor: Color.fromRGBO(59, 62, 91, 1),
+        unselectedItemColor: Colors.grey[300],
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.reorder,size: 35), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite,size: 35), label: "")
+        ],
+      ),
+    );
+  }
+}
